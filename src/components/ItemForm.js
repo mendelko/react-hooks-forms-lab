ca8a7548e4e24142124b71a2b61249a5a7687b90
Mendel <mkonikov123@gmail.com>
2021-07-08 19:05:30 -0400

    commit tree f5ebc4dacfb2c8d25611540d8ab33894978727d2

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
 function ItemForm({ onItemFormSubmit }) {
+
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce")
+
+  function handleNameChange(e){
+    setName(e.target.value);
+    
+  }
+
+  function handleCategoryChange(e){
+    setCategory(e.target.value);
+    
+  }
+
+
+  function handleSubmit(e){
+    console.log(e)
+    e.preventDefault()
+    onItemFormSubmit({ 
+      id: uuid(), // the `uuid` library can be used to generate a unique id
+      name,
+      category,
+    }  )
+  }
+
   return (
-    <form className="NewItem" onSubmit={onItemFormSubmit}>
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input onChange={handleNameChange} type="text" name="name" />
       </label>
 
       <label>
         Category:
-        <select name="category" defaultValue="Produce">
+        <select onChange={handleCategoryChange} name="category" defaultValue="Produce">
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -7,43 +7,20 @@ import { v4 as uuid } from "uuid";
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [search, setSearch] = useState("");
-  const [itemName, setItemName] = useState("");
-  const [itemCategory, setItemCategory] = useState("Produce")
   const [submittedData, setSubmittedData] = useState([...items]);
-  const [error, setErrors] = useState([]);
 
-  function handleSubmit(e){
-    e.preventDefault()
-    console.log(e)
-    if (itemName.length > 0){
-      const newItem = {
-        id: uuid(), // the `uuid` library can be used to generate a unique id
-        name: itemName,
-        category: itemCategory,
-      }; 
-      console.log(newItem, submittedData)
-      const dataArray = [...submittedData, newItem]
-      setSubmittedData(dataArray);
-      setItemName("");
-      setItemCategory("Produce");
-      setErrors([]);
-    } else {
-      setErrors("Item name is required!")
-    }
-  }
+  
+
+  function handleSubmit(newItem) {
+    setSubmittedData([...items, newItem]);
+  } 
 
   function handleSearchChange(event){
     setSearch(event.target.value);
   }
 
-  
 
-
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
-  }
-
-  const itemsToDisplay = items.filter(
+  const itemsToDisplay = submittedData.filter(
       (item) => selectedCategory === "All" || item.category === selectedCategory
     ).filter((item) => item.name.toLowerCase().includes(search.toLowerCase())
   )
@@ -52,7 +29,7 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm onItemFormSubmit={handleSubmit}/>
-      <Filter onSearchChange={handleSearchChange} onCategoryChange={handleCategoryChange} />
+      <Filter onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
